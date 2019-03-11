5590be5f34552e99a5ac6267dec689f257f26e93
Tarun <tarun@nickelfox.com>
2019-03-11 22:35:57 +0530

    feat(quicklock): create quick lock screen

---
+++ app/src/main/java/com/trikh/focuslock/ui/QuickLockActivity.kt
@@ -0,0 +1,13 @@
+package com.trikh.focuslock.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trikh.focuslock.R
+
+class QuickLockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quick_lock)
+    }
+}
