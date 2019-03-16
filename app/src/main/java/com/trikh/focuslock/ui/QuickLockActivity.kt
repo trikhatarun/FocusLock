04ab78d19b272c8c8477c1e865a639b309cb154d
Tarun <tarun@nickelfox.com>
2019-03-16 20:02:48 +0530

    feat(quicklock): add quick lock screen

--- app/src/main/java/com/trikh/focuslock/ui/QuickLockActivity.kt
+++ app/src/main/java/com/trikh/focuslock/ui/QuickLockActivity.kt
@@ -3,11 +3,15 @@ package com.trikh.focuslock.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trikh.focuslock.R
+import kotlinx.android.synthetic.main.activity_quick_lock.*
 
 class QuickLockActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quick_lock)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
