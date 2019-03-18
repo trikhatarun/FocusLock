2804e8e078478ac64fd619dbe39b4270994a68cf
Tarun <tarun@nickelfox.com>
2019-03-18 06:37:10 +0530

    feat(app-picker): add app picker dialog

---
+++ app/src/main/java/com/trikh/focuslock/ui/AppsPickerDialog.kt
@@ -0,0 +1,12 @@
+package com.trikh.focuslock.ui
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+
+class AppsPickerDialog(context: Context) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
