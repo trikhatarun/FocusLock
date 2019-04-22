f2fe47dd9b5ff024f9fc33488d65330ecfa1bd59
Tarun <tarun@nickelfox.com>
2019-04-22 23:21:13 +0530

    feat(navigation): add navigation component

--- app/src/main/java/com/trikh/focuslock/ui/MainActivity.kt
+++ app/src/main/java/com/trikh/focuslock/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.trikh.focuslock.ui
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.trikh.focuslock.R
+import com.trikh.focuslock.ui.schedule.ScheduleFragment
+import com.trikh.focuslock.ui.video.VideoFragment
 import com.trikh.focuslock.widget.extensions.setAppBarLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
