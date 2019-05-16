abda51b6c91a5390ef6b288dbb5ca952eb93f2a2
Karan Nickelfox <karan@nickelfox.com>
2019-05-16 13:12:58 +0530

    feat(add-schedule): add toolbar

--- app/src/main/java/com/trikh/focuslock/ui/schedule/AddScheduleActivity.kt
+++ app/src/main/java/com/trikh/focuslock/ui/schedule/AddScheduleActivity.kt
@@ -3,11 +3,15 @@ package com.trikh.focuslock.ui.schedule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trikh.focuslock.R
+import com.trikh.focuslock.widget.extensions.setAppBarLayout
+import kotlinx.android.synthetic.main.toolbar.*
 
 class AddScheduleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_schedule)
+
+        arcToolbar.setAppBarLayout(appbar)
     }
 }
