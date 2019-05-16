cc81226bd2d533cb21c3d8bb8591bcd1738e884d
Karan Nickelfox <karan@nickelfox.com>
2019-05-16 11:02:29 +0530

    feat(add-schedule): create add schedule activity

---
+++ app/src/main/java/com/trikh/focuslock/ui/schedule/AddScheduleActivity.kt
@@ -0,0 +1,13 @@
+package com.trikh.focuslock.ui.schedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trikh.focuslock.R
+
+class AddScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_schedule)
+    }
+}
