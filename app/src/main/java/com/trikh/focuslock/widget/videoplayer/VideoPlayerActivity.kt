3db4772ce7fcca2d0cf63c53b9c747fbb1ee7dca
Tarun <tarun@nickelfox.com>
2019-05-22 18:13:41 +0530

    feat(video): add video linking to navgraph

---
+++ app/src/main/java/com/trikh/focuslock/widget/videoplayer/VideoPlayerActivity.kt
@@ -0,0 +1,13 @@
+package com.trikh.focuslock.widget.videoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trikh.focuslock.R
+
+class VideoPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_player)
+    }
+}
