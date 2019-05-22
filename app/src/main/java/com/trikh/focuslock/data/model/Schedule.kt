1911eb9a4cdbac55dbdeb0fa40e7d897ea0ff605
Karan Nickelfox <karan@nickelfox.com>
2019-05-22 18:16:14 +0530

    feat(schedule): create schedule class

--- app/src/main/java/com/trikh/focuslock/data/model/Schedule.kt
+++ app/src/main/java/com/trikh/focuslock/data/model/Schedule.kt
@@ -1,1 +1,11 @@
 package com.trikh.focuslock.data.model
+
+import java.util.*
+
+data class Schedule(
+    val id: Int,
+    val startTime: Calendar,
+    val endTime: Calendar,
+    val level: Int,
+    val active: Boolean
+)
