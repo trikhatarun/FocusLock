61535022e97c16d891f9fbbfe84236acd4a1759b
bharat singh <bharat.singh@nickelfox.com>
2019-05-29 18:34:54 +0530

    Feat(CustomDialog): Custom Dialog added

--- app/src/main/java/com/trikh/focuslock/ui/MainActivity.kt
+++ app/src/main/java/com/trikh/focuslock/ui/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.DialogTitle
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.trikh.focuslock.R
@@ -13,15 +16,33 @@ import com.trikh.focuslock.ui.schedule.AddScheduleActivity
 import com.trikh.focuslock.ui.schedule.ScheduleFragment
 import com.trikh.focuslock.ui.settting.SettingsFragment
 import com.trikh.focuslock.widget.arctoolbar.setAppBarLayout
+import com.trikh.focuslock.widget.customdialog.CustomDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteractionListener,
-    SettingsFragment.OnFragmentInteractionListener {
+    SettingsFragment.OnFragmentInteractionListener{
 
     override fun onFragmentInteraction(uri: Uri) {
 
     }
+
+    /*private fun showDialog(title: String, yesText: String, noText: String){
+        val dialog = CustomDialog(title,noText,yesText)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        dialog.show(fragmentTransaction,"")
+    }
+
+    override fun onSelectYes() {
+        Toast.makeText(this,"Yes is Clicked",Toast.LENGTH_SHORT).show()
+
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSelectNo() {
+        Toast.makeText(this,"No is Clicked",Toast.LENGTH_SHORT).show()
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +52,7 @@ class MainActivity : AppCompatActivity(), ScheduleFragment.OnFragmentInteraction
 
         bottomNavigationBar.itemIconTintList = null
         bottomNavigationBar.setupWithNavController(findNavController(R.id.container))
+        //showDialog("Are you really want to unblock the apps","YES", "NO")
     }
 
     fun onAddScheduleClick(v: View) {
--- app/src/main/java/com/trikh/focuslock/widget/customdialog/CustomDialog.kt
+++ app/src/main/java/com/trikh/focuslock/widget/customdialog/CustomDialog.kt
@@ -1,27 +1,37 @@
 package com.trikh.focuslock.widget.customdialog
 
+import android.app.Dialog
 import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.trikh.focuslock.R
 
-class CustomDialog(private val title : String): DialogFragment(){
+class CustomDialog(private val title : String, private val noButtonText : String, private val yesButtonText : String ): DialogFragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.custom_dialog_layout,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        dialog.setCancelable(false)
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        var colorDrawable = ColorDrawable(Color.TRANSPARENT)
+        dialog?.window?.setBackgroundDrawable(colorDrawable)
         val titleTextView = view.findViewById<TextView>(R.id.title_textview)
         titleTextView.setText(title)
         val buttonNo = view.findViewById<TextView>(R.id.no_button)
+        buttonNo.setText(noButtonText)
         val buttonYes =view.findViewById<TextView>(R.id.yes_button)
+        buttonYes.setText(yesButtonText)
 
         buttonNo.setOnClickListener {
             val dialogListener = activity as DialogListener?
