34bb9dc0048aff054662764f89830a7a0a9288da
Karan Nickelfox <karan@nickelfox.com>
2019-05-31 13:05:47 +0530

    feat(app-block): create app block activity

---
+++ app/src/main/java/com/trikh/focuslock/ui/blockedapp/BlockedAppActivity.kt
@@ -0,0 +1,13 @@
+package com.trikh.focuslock.ui.blockedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trikh.focuslock.R
+
+class BlockedAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blocked_app)
+    }
+}
