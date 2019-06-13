6226834b8f0e2cb6f33f7450837814a90ea80977
Karan Nickelfox <karan@nickelfox.com>
2019-06-13 14:08:00 +0530

    feat(video): load video thumbnail

--- app/src/main/java/com/trikh/focuslock/data/source/local/VideoLocalRepository.kt
+++ app/src/main/java/com/trikh/focuslock/data/source/local/VideoLocalRepository.kt
@@ -4,12 +4,16 @@ package com.trikh.focuslock.data.source.local
 import java.util.*
 
 class VideoLocalRepository {
-    val videoIds = arrayOf<String>()
+    val videoIds = arrayOf<String>("ISZCSikwSlI")
 
     fun getTodaysVideoId(): String {
         val calendar = Calendar.getInstance()
         val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
-        return videoIds[dayOfMonth]
+        if (videoIds.size >= 31) {
+            return videoIds[dayOfMonth]
+        } else {
+            return videoIds.random()
+        }
     }
 
     companion object {
--- app/src/main/java/com/trikh/focuslock/ui/video/VideoFragment.kt
+++ app/src/main/java/com/trikh/focuslock/ui/video/VideoFragment.kt
@@ -1,19 +1,30 @@
 package com.trikh.focuslock.ui.video
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
 import com.trikh.focuslock.R
+import com.trikh.focuslock.data.source.local.VideoLocalRepository
+import kotlinx.android.synthetic.main.fragment_video.*
 
 class VideoFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_video, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val url = getVideoUrl(VideoLocalRepository.getInstance().getTodaysVideoId())
+        Picasso.get().load(url).fit().centerCrop().into(videoThumbnail)
+    }
+
+    fun getVideoUrl(videoId: String): String {
+        return "http://img.youtube.com/vi/$videoId/hqdefault.jpg"
     }
 }
