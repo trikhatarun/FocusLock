5b97a619c3c65cab6017f36ec74f2460dcaa7fd6
Karan Nickelfox <karan@nickelfox.com>
2019-06-14 13:25:28 +0530

    feat(onboarding): create onboarding activity

---
+++ app/src/main/java/com/trikh/focuslock/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+package com.trikh.focuslock.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trikh.focuslock.R
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
