22b2333127ec341782af1dc572752b302194f20c
bharat singh <bharat.singh@nickelfox.com>
2019-06-18 17:34:38 +0530

    refactor(Utils):remove unused code

--- app/src/main/java/com/trikh/focuslock/data/utils/ListConverters.kt
+++ app/src/main/java/com/trikh/focuslock/data/utils/ListConverters.kt
@@ -2,42 +2,11 @@ package com.trikh.focuslock.data.utils
 
 import android.util.Log
 import com.google.gson.Gson
+import com.trikh.focuslock.widget.app_picker.AppInfo
 
 class ListConverters {
     companion object {
 
-        fun listToString(value: List<String>): String {
-            val st = Gson().toJson(value) as String
-            Log.d("ListConverter: ", " String: $st")
-            return st
-        }
-
-
-        fun stringToList(value: String): List<String> {
-            Log.d("ListConverter: ", " String TO Convert: ${value}")
-            val objects = Gson().fromJson(value, Array<String>::class.java)
-            Log.d("ListConverter: ", " AppList: ${objects}")
-            return objects.asList()
-        }
-
-
-        fun listToArray(appList: List<String>): Array<String> {
-            var array = arrayOfNulls<String>(appList.size)
-            for (i in 0 until appList.size) {
-                array[i] = appList[i]
-            }
-            Log.d("ListConverter: ", " Array: $array")
-            return array as Array<String>
-        }
-
-        fun arrayToList(array: Array<String>): List<String> {
-            var list = ArrayList<String>()
-            for (i in 0 until array.size) {
-                 list[i] = array[i]
-            }
-            Log.d("ListConverter: ", " AppList: $list")
-            return list
-        }
 
         fun fetchList(appList: List<String>): List<String> {
             Log.d("ListConverter: ", " AppList: $appList")
