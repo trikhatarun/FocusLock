c7c5be5f97931a7196c620c467085e19bc32bc81
bharat singh <bharat.singh@nickelfox.com>
2019-06-19 14:03:10 +0530

    refactor(onboarding):add onclick in ViewModel

--- app/src/main/java/com/trikh/focuslock/ui/onboarding/TimePickerViewModel.kt
+++ app/src/main/java/com/trikh/focuslock/ui/onboarding/TimePickerViewModel.kt
@@ -1,11 +1,33 @@
 package com.trikh.focuslock.ui.onboarding
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.trikh.focuslock.Application
+import com.trikh.focuslock.R
+import com.trikh.focuslock.utils.TimeDurationUtils
+import java.util.*
 
 class TimePickerViewModel : ViewModel() {
     val hours: MutableLiveData<Int> = MutableLiveData()
     val minutes: MutableLiveData<Int> = MutableLiveData()
     val sleepHours: MutableLiveData<Int> = MutableLiveData<Int>().apply { value = 0 }
+    //val blockText: MutableLiveData<String> = MutableLiveData< String>().apply{ value = Application.instance.getString(R.id.blocked_apps_title)}
+
+
+    fun onClick(){
+        val startTime = Calendar.getInstance()
+        val endTime = Calendar.getInstance()
+        startTime.set(Calendar.HOUR, hours.value!!)
+        startTime.set(Calendar.MINUTE, minutes.value!!)
+
+        endTime.set(Calendar.HOUR, sleepHours.value!!)
+        val duration = TimeDurationUtils.calculateDuration(startTime,endTime)
+        Log.d("TimePickerViewModel:"," duration: $duration")
+
+
+    }
+
 
 }
