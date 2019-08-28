7cdba0d9171a58baf094a2805cc8084fcca5e238
karan-nickelfox <karan@nickelfox.com>
2019-08-28 20:13:46 +0530

    trivial commit

--- app/src/main/java/com/trikh/focuslock/widget/app_picker/AppPickerDialog.kt
+++ app/src/main/java/com/trikh/focuslock/widget/app_picker/AppPickerDialog.kt
@@ -1,11 +1,11 @@
 package com.trikh.focuslock.widget.app_picker
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -16,11 +16,18 @@ import com.trikh.focuslock.utils.ViewModelFactory
 import kotlinx.android.synthetic.main.app_picker_dialog.*
 
 
-class AppPickerDialog(private val selectedAppList: List<AppInfo>, private val interactionListener: InteractionListener) : DialogFragment() {
+class AppPickerDialog(
+    private val selectedAppList: List<AppInfo>,
+    private val interactionListener: InteractionListener
+) : DialogFragment() {
 
     private val applicationListAdapter = AppsAdapter(ArrayList())
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return LayoutInflater.from(context).inflate(R.layout.app_picker_dialog, container)
     }
 
@@ -31,14 +38,18 @@ class AppPickerDialog(private val selectedAppList: List<AppInfo>, private val in
         appsRV.layoutManager = AutoFitGridLayoutManager(view.context, 70)
 
         appsRV.adapter = applicationListAdapter
-        val columns = (appsRV.layoutManager as AutoFitGridLayoutManager).getColumnCountForAccessibility(appsRV.Recycler(), RecyclerView.State())
-        Log.e("AppPickerDialog: ","Columns: $columns")
+        val columns =
+            (appsRV.layoutManager as AutoFitGridLayoutManager).getColumnCountForAccessibility(
+                appsRV.Recycler(),
+                RecyclerView.State()
+            )
+
         ViewModelProviders.of(this, ViewModelFactory(activity!!.application, selectedAppList))
-                .get(AppListViewModel::class.java)
-                .getAppInfoList()
-                .observe(this, Observer { appList ->
-                    applicationListAdapter.updateList(appList)
-                })
+            .get(AppListViewModel::class.java)
+            .getAppInfoList()
+            .observe(this, Observer { appList ->
+                applicationListAdapter.updateList(appList)
+            })
 
         confirmationButton.setOnClickListener {
             interactionListener.onConfirm(applicationListAdapter.getSelectedApplicationList())
@@ -48,18 +59,31 @@ class AppPickerDialog(private val selectedAppList: List<AppInfo>, private val in
         cancelButton.setOnClickListener {
             dismiss()
         }
+
+        appsSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                //Do nothing
+                return true
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                applicationListAdapter.filter(query)
+                return true
+            }
+
+        })
     }
 
     override fun onResume() {
         super.onResume()
         val dialog = dialog
         dialog?.window?.setLayout(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
         )
     }
 
-    public interface InteractionListener {
+    interface InteractionListener {
         fun onConfirm(applicationList: List<AppInfo>)
     }
 }
--- app/src/main/java/com/trikh/focuslock/widget/app_picker/AppsAdapter.kt
+++ app/src/main/java/com/trikh/focuslock/widget/app_picker/AppsAdapter.kt
@@ -18,19 +18,26 @@ import kotlinx.android.synthetic.main.app_list_item.view.*
 
 class AppsAdapter(private var applicationList: List<AppInfo>) : RecyclerView.Adapter<AppsAdapter.ApplicationViewHolder>() {
 
+    private var filteredList = mutableListOf<AppInfo>()
+
+    init {
+        filteredList.addAll(applicationList)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApplicationViewHolder {
         return ApplicationViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.app_list_item, parent, false))
     }
 
-    override fun getItemCount() = applicationList.size
+    override fun getItemCount() = filteredList.size
 
 
     override fun onBindViewHolder(holder: ApplicationViewHolder, position: Int) {
-        holder.bind(applicationList[position])
+        holder.bind(filteredList[position])
     }
 
     fun updateList(applicationList: List<AppInfo>) {
         this.applicationList = applicationList
+        this.filteredList = applicationList.toMutableList()
         notifyDataSetChanged()
     }
 
@@ -45,11 +52,21 @@ class AppsAdapter(private var applicationList: List<AppInfo>) : RecyclerView.Ada
         return selectedAppsList
     }
 
+    fun filter(query: String?) {
+        if (!query.isNullOrEmpty()) {
+            filteredList = applicationList.filter { it.name.toLowerCase().contains(query.toLowerCase()) }.toMutableList()
+        } else {
+            filteredList.clear()
+            filteredList.addAll(applicationList)
+        }
+        notifyDataSetChanged()
+    }
+
     inner class ApplicationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
         View.OnClickListener {
         override fun onClick(v: View?) {
             updateData()
-            updateView(applicationList[adapterPosition].blocked)
+            updateView(filteredList[adapterPosition].blocked)
         }
 
         fun bind(appInfo: AppInfo) {
@@ -78,7 +95,10 @@ class AppsAdapter(private var applicationList: List<AppInfo>) : RecyclerView.Ada
         }
 
         private fun updateData() {
-            applicationList[adapterPosition].blocked = !applicationList[adapterPosition].blocked
+            applicationList.find { it.packageName == filteredList[adapterPosition].packageName }?.let {
+                it.blocked = !it.blocked
+            }
+            filteredList[adapterPosition].blocked = !filteredList[adapterPosition].blocked
         }
     }
 }
