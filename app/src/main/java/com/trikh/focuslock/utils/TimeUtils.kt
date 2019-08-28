25dd719d65a8464f4dd9e0e9fb0e59919dd4ebff
karan-nickelfox <karan@nickelfox.com>
2019-08-28 09:42:54 +0530

    feat(home): fix schedule time

--- app/src/main/java/com/trikh/focuslock/utils/TimeUtils.kt
+++ app/src/main/java/com/trikh/focuslock/utils/TimeUtils.kt
@@ -9,14 +9,12 @@ object TimeUtils {
     fun getSleepTime(time: Date, level: Int): String? {
         val sleepTime = Calendar.getInstance()
         sleepTime.time = time
-        sleepTime.add(Calendar.MINUTE, -(level * 30))
         return timeFormat.format(sleepTime.time)
     }
 
     fun getAwakeTime(time: Date, level: Int): String? {
         val awakeTime = Calendar.getInstance()
         awakeTime.time = time
-        awakeTime.add(Calendar.MINUTE, (level * 60))
         return timeFormat.format(awakeTime.time)
     }
 }
