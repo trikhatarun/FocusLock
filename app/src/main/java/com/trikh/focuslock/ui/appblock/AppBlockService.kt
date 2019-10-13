1a261c7050ffa2470e113258e1daa66b41c50d84
Tarun Trikha <tarun@nickelfox.com>
2019-10-13 15:14:04 +0530

    refactor(service): refactor app block service

--- app/src/main/java/com/trikh/focuslock/ui/appblock/AppBlockService.kt
+++ app/src/main/java/com/trikh/focuslock/ui/appblock/AppBlockService.kt
@@ -7,28 +7,18 @@ import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.trikh.focuslock.Application
 import com.trikh.focuslock.R
-import com.trikh.focuslock.data.model.InstantLockSchedule
 import com.trikh.focuslock.data.source.ScheduleRepository
-import com.trikh.focuslock.utils.Constants.Companion.DAILY_SCHEDULE
-import com.trikh.focuslock.utils.Constants.Companion.INSTANT_LOCK
 import com.trikh.focuslock.utils.Constants.Companion.NOTIFICATION_CHANNEL_ID
 import com.trikh.focuslock.utils.Constants.Companion.PACKAGE_NAME
-import com.trikh.focuslock.utils.Constants.Companion.SCHEDULE
-import com.trikh.focuslock.utils.Constants.Companion.SCHEDULE_TYPE
 import com.trikh.focuslock.utils.Constants.Companion.SERVICE_ID
 import com.trikh.focuslock.utils.Constants.Companion.TIME_INTERVAL
 import io.reactivex.Observable
-import io.reactivex.ObservableSource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.BiFunction
-import io.reactivex.functions.Function
-import io.reactivex.functions.Function3
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
@@ -61,81 +51,32 @@ class AppBlockService : Service() {
 
         scheduleRepository.getBlockedPackages().subscribeBy {
             blockedPackages = it
-            runningTimeObservable.subscribe{
-                if (blockedPackages.isEmpty()){
-                    runningTime = 0
-                }else{
-                runningTime = it
+            runningTimeObservable.subscribe{time ->
+                runningTime = if (blockedPackages.isEmpty()) {
+                    0
+                } else {
+                    time
                 }
                 setTimeAndPackages(time = runningTime, packages = blockedPackages)
             }
-        }
-
-        //val blockedPackageObservable = scheduleRepository.getInstantLockBlockedPackages()
-
-        /*blockedPackageObservable.subscribeBy {
-            blockedPackages= it
-            setTimeAndPackages(runningTime, blockedPackages)
-            runningTimeObservable.subscribe{
-                runningTime = it
-            }
-        }*/
-
-
-
-
-
-        /*scheduleRepository.getScheduleBlockedPackages().subscribeBy {
-
-        }*/
-
-
-        /*Observable.zip(
-            runningTimeObservable,
-            blockedPackageObservable,
-            BiFunction { t1: Long, t2: List<String> ->
-                setTimeAndPackages(t1, t2) }
-        ).subscribe()*/
-
-        /*scheduleRepository.getScheduleBlockedPackages()
-            .mergeWith(scheduleRepository.getInstantLockBlockedPackages())
-            .subscribe{
-                blockedPackages = it
-                runningTimeObservable.subscribe{
-                    runningTime = it
-                    setTimeAndPackages(time = runningTime, packages = blockedPackages)
-                }
-
-            }*/
-
-
-
-
+        }.addTo(compositeDisposable)
     }
 
     private fun setTimeAndPackages(time: Long, packages: List<String>) {
         runningTime = time
         val cal = Calendar.getInstance()
         cal.timeInMillis = 600000
-        val minute =cal.get(Calendar.MINUTE)
-        val hours = cal.get(Calendar.HOUR_OF_DAY)
-        Log.d("SetTimeAndPackages:", "Hours: $hours   Minutes: $minute")
+
         blockedPackages = packages
 
         if (runningTime > 0) {
-
-
             setInterval()
-
         } else {
             stopSelf()
         }
     }
 
-
     private fun setInterval() {
-
-
         //checking is blocked app is opening
         Observable.interval(TIME_INTERVAL, TimeUnit.MILLISECONDS)
             .subscribeOn(Schedulers.computation())
@@ -143,18 +84,20 @@ class AppBlockService : Service() {
             .filter { blockedPackages.contains(it) && it != packageName }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { p ->
-                val i = Intent(Intent.ACTION_MAIN)
-                i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                i.addCategory(Intent.CATEGORY_HOME)
-                this.startActivity(i)
-                val blockedAppIntent = Intent(this, BlockedAppActivity::class.java)
-                blockedAppIntent.putExtra(PACKAGE_NAME, p)
-                blockedAppIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                val homeIntent = Intent(Intent.ACTION_MAIN).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    addCategory(Intent.CATEGORY_HOME)
+                }
+                startActivity(homeIntent)
+
+                val blockedAppIntent = Intent(this, BlockedAppActivity::class.java).apply {
+                    putExtra(PACKAGE_NAME, p)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
                 startActivity(blockedAppIntent)
+
                 activityManager.killBackgroundProcesses(p)
             }.addTo(compositeDisposable)
-
-
         //timer for the smallest endTime
         Observable.timer(runningTime, TimeUnit.MILLISECONDS)
             .subscribeOn(Schedulers.computation())
